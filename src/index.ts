49a134d9078ecd61653d5be8129f25590781e441
Anh Tang <anh.tang@opswat.com>
2024-11-19 12:28:43 +0700

    deploy heroku

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,39 @@
 import express from 'express';
+import cors from 'cors';
 import { createServer } from 'http';
+import { Server } from 'socket.io';
 const app = express();
 const server = createServer(app);
-import WebSocket, { WebSocketServer } from 'ws';
 const PORT = process.env.PORT ?? 8080;
-//@ts-ignore
-const wss = new WebSocket.Server({ port: PORT });
-wss.on('connection', (ws: WebSocket) => {
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ["Content-Type"],
+        credentials: true
+    }
+});
+io.on('connection', (socket) => {
     console.log('WebSocket connection established');
-
-    ws.on('message', (message: any) => {
+    socket.emit('message', 'Connected!');
+    socket.on('message', (message: any) => {
         console.log('Received message:', JSON.parse(message));
     });
 
-    ws.on('close', () => {
+    socket.on('close', () => {
         console.log('Client disconnected');
     });
 
-    ws.on('error', (error: any) => {
+    socket.on('error', (error: any) => {
         console.error('WebSocket error:', error);
     });
+});
+
+app.get('/', (req, res) => {
+    res.json({ success: true })
+})
+
+server.listen(PORT, () => {
+    console.log(`'server running at port ${PORT}'`);
 });
 module.exports = app;
